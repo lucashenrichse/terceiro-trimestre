0185457ac836ad97e15d5f54132aec15bce95c29
lucashenrichse <127400839+lucashenrichse@users.noreply.github.com>
2024-10-23 09:09:26 -0300

    Add files via upload

---
+++ arquivosglobais.js
@@ -0,0 +1,37 @@
+consturl = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+}
+
+vizualizarInformacoesGlobais()
+const u = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformacoesGlobais() {
+
+        const res = await fetch(url)
+
+        const dados = await res.json()
+
+        console.log(dados);
+
+    }
+
+    visualizarInformacoesGlobais()
+    async function vizualizarInformacoesGlobais() {
+        constres = await fetch(url)
+        const dados await res .json
+        const paragrafodocument.createElement ('p')
+        paragrafo.classList.add('graficos-container__texto')
+       paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+        console.log(paragrafo)
+      }
+      
+      vizualizarInformacoesGlobais()
+      
+
+
+
+
